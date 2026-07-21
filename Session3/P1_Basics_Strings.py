21c9cf7dac788e01580040c73539abe8b50cdebe
Azaan Suhail <azaan.suhail@gmail.com>
2026-07-21 23:22:12 +0530

    Session 3

---
+++ Session3/P1_Basics_Strings.py
@@ -0,0 +1,18 @@
+name="Kiaan"
+tutor_name="Azaan"
+tutor_age="24"
+random="1@#%$^&_ Azaan & Kiaan"
+age=14
+
+print(name)
+print(tutor_name)
+print(tutor_age)
+print(random)
+
+
+#To check the string (proof)
+print(type(name))
+print(type(tutor_name))
+print(type(tutor_age))
+print(type(random))
+print(type(age))
